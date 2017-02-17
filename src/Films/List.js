897eadeac830640162ee1d0f04ccea199a69bf8f
Roman Zanettin <roman.zanettin@amiadogroup.com>
2017-02-17 17:26:47 +0100

    fixed typo

--- src/Films/List.js
+++ src/Films/List.js
@@ -66,7 +66,7 @@ export default compose(
     renderComponent(Error),
   ),
 
-  // wile loading, we want to show a loading screen
+  // while loading, we want to show a loading screen
   // to our users, so we compose another branch
   // which checks the loading state
   branch(
